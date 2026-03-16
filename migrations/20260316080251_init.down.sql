4139f98e92fe0225522b37aa0154aa2d9a93e1df
karl robeck alferez <karlalferezfx@gmail.com>
2026-03-16 16:57:55 +0800

    refactor migration scripts and update file management UI structure

--- migrations/20260316080251_init.down.sql
+++ migrations/20260316080251_init.down.sql
@@ -1,2 +1,4 @@
 -- Add down migration script here
+drop table if exists file_folder;
+drop table if exists folders;
 drop table if exists files;
--- migrations/20260316080251_init.up.sql
+++ migrations/20260316080251_init.up.sql
@@ -7,3 +7,19 @@ create table "files" (
   created_at text not null default current_timestamp,
   updated_at text not null default current_timestamp
 );
+
+create table "folders" (
+  id integer primary key autoincrement,
+  name text not null,
+  path text not null,
+  created_at text not null default current_timestamp,
+  updated_at text not null default current_timestamp
+);
+
+create table "file_folder" (
+  file_id integer not null,
+  folder_id integer not null,
+  primary key (file_id, folder_id),
+  foreign key (file_id) references files(id) on delete cascade,
+  foreign key (folder_id) references folders(id) on delete cascade
+);
